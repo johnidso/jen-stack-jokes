f93851ccc8f3500291fe4cff03f11e66d674d948
John Idso <john.d.idso@gmail.com>
2021-06-28 13:15:23 -0500

    outlined joke and started on client side post

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -5,3 +5,32 @@ $( document ).ready( onReady );
 function onReady() {
     console.log('DOM ready');
 }
+
+
+// show all jokes on page load
+// user can add jokes using input form 
+//  whoseJoke:
+//  jokeQuestion:
+//  punchLine:
+// after joke is added, re-display on DOM 
+
+function sendJoke(){
+    let author = $('#whoseJokeIn').val();
+    let joke = $('#questionIn').val();
+    let punchLine = $('#punchlineIn').val();
+    $.ajax({
+        method: 'POST',
+        url: '/joke',
+        data: {
+            whoseJoke: author,
+            jokeQuestion: joke,
+            punchLine: punchLine
+        }
+    })
+    .then(() => {
+        // get jokes
+    })
+    .catch((error) => {
+        alert('Error!', error);
+    })
+}
